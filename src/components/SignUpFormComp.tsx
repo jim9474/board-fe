8773b9f41e08ea5ef5ef4aeb9729d50de545704e
jim9474 <jim9474@naver.com>
2025-06-09 17:37:39 +0900

    Feat: 회원가입 기능 추가

--- src/components/SignUpFormComp.tsx
+++ src/components/SignUpFormComp.tsx
@@ -4,12 +4,14 @@ import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
 import { Col } from 'react-bootstrap';
 import useApi from '../apis/Api';
+import { useNavigate } from 'react-router-dom';
 
 interface UserCreateInfo {
     userId: string;
     userPw: string;
     userEmail: string;
     userNn: string;
+    confirmPw: string;
 }
 
 const SignUpFormComp = () => {
@@ -18,8 +20,12 @@ const SignUpFormComp = () => {
         userId: '',
         userPw: '',
         userEmail: '',
-        userNn: ''
+        userNn: '',
+        confirmPw: ''
     });
+    const isFilled = formData.userPw && formData.confirmPw;
+    const isMatch = formData.userPw === formData.confirmPw;
+    const navigate = useNavigate();
 
     const handleIdCheck = () => {
         useApi.get(`/getCheckId?userId=${formData.userId}`)
@@ -36,15 +42,41 @@ const SignUpFormComp = () => {
         });
     };
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const isValidForm = (data: UserCreateInfo) => {
+        return (
+            idStatus === 'valid' &&
+            data.userPw.trim() !== '' &&
+            data.userEmail.trim() !== '' &&
+            data.userNn.trim() !== '' &&
+            isMatch
+        );
+    };
+
+    const handleSubmit = (e: React.FormEvent, formData: UserCreateInfo) => {
         e.preventDefault();
+        const confirmed = window.confirm("가입하시겠습니까?");
+        if (!confirmed) return;
+
         console.log('formData >>> ', formData);
+        if (isValidForm(formData)) {
+            useApi.post('/createUser', formData)
+            .then(res => {
+                alert("가입 완료");
+                navigate("/");
+            })
+            .catch(err => {
+                alert('가입 실패 '+err);
+            });
+        } else {
+            alert("모든 항목을 입력해주세요.");
+        }
+
     };
 
     return (
         <>
         <h2 className='signup'>회원가입</h2>
-        <Form className='list mx-auto w-50' onSubmit={handleSubmit}>
+        <Form className='list mx-auto w-50' onSubmit={(e) => handleSubmit(e, formData)}>
             <Form.Group className='mb-3' controlId="formGridId">
                 <Form.Label>ID</Form.Label>
                 <Form.Control 
@@ -54,7 +86,7 @@ const SignUpFormComp = () => {
                 />
                 <Row className="">  
                     <Col xs="auto">
-                        <button onClick={handleIdCheck}>중복확인</button>
+                        <button type='button' onClick={handleIdCheck}>중복확인</button>
                     </Col>
                     <Col>
                         {idStatus === 'valid' && (
@@ -80,7 +112,17 @@ const SignUpFormComp = () => {
 
             <Form.Group className="mb-3" controlId="formGridConfirmPassword">
                 <Form.Label>Confirm Password</Form.Label>
-                <Form.Control type="password" placeholder="Confirm Password" />
+                <Form.Control 
+                    type="password" 
+                    placeholder="Confirm Password"
+                    value={formData.confirmPw}
+                    onChange={(e) => setFormData({...formData, confirmPw: e.target.value})} 
+                />
+                {isFilled && (
+                    <span style={{ color: isMatch ? 'green' : 'red'}}>
+                        {isMatch ? '✔ 비밀번호가 동일합니다.' : '✘ 비밀번호가 동일하지 않습니다.'}
+                    </span>
+                )}
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formGridEmail">
